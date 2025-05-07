1f8937f80fa32a1c4734b662b24535043eaeaf59
Felipe Jordani <faljordani@gmail.com>
2025-05-07 08:01:49 +0200

    test and code for delete accounts

--- service/routes.py
+++ service/routes.py
@@ -111,8 +111,22 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
 
-# ... place you code here to DELETE an account ...
+    # use the Account.find() method to retrieve the account by the account_id
+    account = Account.find(account_id)
+    if account:
+        # if found, call the delete() method on the account
+        account.delete()
+
+    # return an empty body ("") with a return code of status.HTTP_204_NO_CONTENT
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
